4ea10a5d0c7c77e9a3982d723270b7f3e3eded9d
jovialcore <jollydosky@gmail.com>
2021-12-03 16:58:23 +0100

    I am done with promotion though for now

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -10,11 +10,14 @@ class PromotionController extends Controller
     public function promote(Request  $request)
     {
 
-        $matches = collect([1, 2]);
-        
-            Student::whereIn('class_id', $matches)->increment('class_id', 1);
-            //I am honestly surprise why using $key to compare 'class_id' in the where clause works perfectly than using the $item which is the actual item as I thought
-    
+        $matches = collect([1, 2,3,4,5,6]);
+        //to avoid this stuff for repeating more than once in year, you will have to check for the session date in the database, against the current year, then fire the increment() stuff
+        Student::whereIn('class_id', $matches)->increment('class_id', 1);
+        //$h->toSql(); using this to output raw sql queries
+        //I am honestly surprise why using $key to compare 'class_id' in the where clause works perfectly than using the $item which is the actual item as I thought
+
+        // DB::table('users')->increment('votes', 1, ['name' => 'John']);
+
         return "I pray this stuff works";
     }
 }
