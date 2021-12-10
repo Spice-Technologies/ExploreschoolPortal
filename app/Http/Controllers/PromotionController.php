5bdf7ec525d8d77120dc81ee8bffc267d84b598f
jovialcore <jollydosky@gmail.com>
2021-12-10 12:44:17 +0100

    fixed the harburger having issues

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -13,29 +13,23 @@ class PromotionController extends Controller
         return view('backend.class.promote');
     }
 
-
-
     public function promote(Request  $request)
     {
+        
         //check if there are ss3 students and move them to a new table
-
         $Student = new Student();
         $Student->where('class_id', 6)->each(function ($finalist) {
             $graduateStud = $finalist->replicate();
             $graduateStud->setTable('graduates');
             $graduateStud->save();
 
-            $finalist->delete();
+            $finalist->delete(); //delete the student from the old tbale (student)
         });
 
         $matches = collect([1, 2, 3, 4, 5, 6]);
         //to avoid this stuff for repeating more than once in year, you will have to check for the session date in the database, against the current year, then fire the increment() stuff
         Student::whereIn('class_id', $matches)->increment('class_id', 1);
         //$h->toSql(); using this to output raw sql queries
-        //I am honestly surprise why using $key to compare 'class_id' in the where clause works perfectly than using the $item which is the actual item as I thought
-
-        // DB::table('users')->increment('votes', 1, ['name' => 'John']);
-
         return redirect()->route('student.index');
     }
 }
