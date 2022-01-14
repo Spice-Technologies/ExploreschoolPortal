4b7572ae2dc0c146a67b9e3f43984e9bfa630bcd
jovialcore <jollydosky@gmail.com>
2022-01-14 20:09:21 +0100

    workin on import

--- app/Models/result.php
+++ app/Models/result.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class result extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['student_id', 'class_id', 'assessment_total', 'exam_score', 'total_score', 'subject_id', 'session_id', 'term_id', 'school_id'];
+
+    
+    public function admin()
+    {
+        return $this->hasMany(Admin::class);
+    }
+    public function pin()
+    {
+
+        return $this->hasOne(Pin::class);
+    }
 }
