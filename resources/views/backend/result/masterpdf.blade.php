d80eee8d478473f93f13eda4bbe045377ea71ef3
jovialcore <jollydosky@gmail.com>
2022-01-28 15:47:17 +0100

    workin on the master sheet ot fix stuff

--- resources/views/backend/result/masterpdf.blade.php
+++ resources/views/backend/result/masterpdf.blade.php
@@ -27,86 +27,34 @@
             <div class="card-body">
                 <div class="table-responsive">
                     <div>
-                        <table id="myTable" class="table align-items-center table-dark">
-                            <thead class="thead-dark">
+                        <table class="table">
+                            <thead>
                                 <tr>
-                                    <th scope="col" class="sort" data-sort="name">Name</th>
-                                    <th scope="col" class="sort" data-sort="budget">Reg Number</th>
-                                    <th scope="col" class="sort" data-sort="status">Status</th>
-                                    <th scope="col">Users</th>
-                                    <th scope="col" class="sort" data-sort="completion">Completion</th>
-                                    <th scope="col"></th>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
                                 </tr>
                             </thead>
-                            <tbody class="list">
-
-
-                                @foreach ($results as $result)
-                                    <tr>
-                                        <th scope="row">
-                                            <div class="media align-items-center">
-                                                <a href="#" class="avatar rounded-circle mr-3">
-                                                    <img alt="Image placeholder" src="../../assets/img/theme/bootstrap.jpg">
-                                                </a>
-                                                <div class="media-body">
-                                                    <span class="name mb-0 text-sm">{{ $result->subject }}</span>
-                                                </div>
-                                            </div>
-                                        </th>
-                                        <td class="budget">
-                                            {{ $result->total_score }}
-                                        </td>
-                                        <td>
-                                            {{ $result->total_score }}
-                                            {{-- <span class="badge badge-dot mr-4">
-                                                <i class="bg-warning"></i>
-                                                <a href="{{ route('student.edit', $studentClass->id) }}"> <span
-                                                        class="status">edit</span> </a>
-                                            </span> --}}
-                                        </td>
-                                        <td>
-                                            <div class="avatar-group">
-                                                <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip"
-                                                    data-original-title="Ryan Tompson">
-                                                    <img alt="Image placeholder" src="../../assets/img/theme/team-1.jpg">
-                                                </a>
-                                                <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip"
-                                                    data-original-title="Romina Hadid">
-                                                    <img alt="Image placeholder" src="../../assets/img/theme/team-2.jpg">
-                                                </a>
-                                                <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip"
-                                                    data-original-title="Alexander Smith">
-                                                    <img alt="Image placeholder" src="../../assets/img/theme/team-3.jpg">
-                                                </a>
-                                                <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip"
-                                                    data-original-title="Jessica Doe">
-                                                    <img alt="Image placeholder"
-                                                        src="{{ asset('assets/img/theme/team-4.jpg') }}">
-                                                </a>
-                                            </div>
-
-                                        </td>
-                                        <td>
-                                            <div class="d-flex align-items-center">
-                                                <span class="completio mr-2"> {{ $result->total_score }} </span>
-                                            </div>
-                                        </td>
-                                        <td class="text-right">
-                                            <div class="dropdown">
-                                                <a class="btn btn-sm btn-icon-only text-light" href="#" role="button"
-                                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    <i class="fas fa-ellipsis-v"></i>
-                                                </a>
-                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                                    <a class="dropdown-item" href="#">Action</a>
-                                                    <a class="dropdown-item" href="#">Another action</a>
-                                                    <a class="dropdown-item" href="#">Something else here</a>
-                                                </div>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
-
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
@@ -116,9 +64,5 @@
             </div>
         </div>
     </div>
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('#myTable').DataTable();
-        });
-    </script>
+
 @endsection
