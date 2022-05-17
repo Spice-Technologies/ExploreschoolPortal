9c1878dc12955abe3671bdc15a82aaf3e75f9f03
mimigraphy20 <jollydosky@gmail.com>
2022-05-17 17:59:19 +0100

    tyring to fix the table to look like what it should be

--- resources/views/dashboard/Student/checkResult/create.blade.php
+++ resources/views/dashboard/Student/checkResult/create.blade.php
@@ -64,16 +64,43 @@
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
-                        <div class="modal-body">
-                            <div class="card col-12 mx-auto">
-                                <div class="card-body ">
+                        <div class="modal-body">    
                                     @if (Session::get('results'))
                                         @foreach (Session::get('results') as $result)
                                             {{ $result->subjectModel->subject }}
                                         @endforeach
                                     @endif
-                                </div>
-                            </div>
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">First</th>
+                                                <th scope="col">Last</th>
+                                                <th scope="col">Handle</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">1</th>
+                                                <td>Mark</td>
+                                                <td>Otto</td>
+                                                <td>@mdo</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">2</th>
+                                                <td>Jacob</td>
+                                                <td>Thornton</td>
+                                                <td>@fat</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">3</th>
+                                                <td colspan="2">Larry the Bird</td>
+                                                <td>@twitter</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                
+                         
                         </div>
                         <div class="modal-footer">
                             {{-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> --}}
