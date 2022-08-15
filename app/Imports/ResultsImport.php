2386006159fbbb61f92065741f934baf879d6e53
mimigraphy20 <jollydosky@gmail.com>
2022-08-15 14:43:13 +0100

    updating result upload feature..Refactoring...etc

--- app/Imports/ResultsImport.php
+++ app/Imports/ResultsImport.php
@@ -16,18 +16,23 @@ class ResultsImport implements ToCollection
      * @return \Illuminate\Database\Eloquent\Model|null
      */
 
-    public $studentInfo = [];
+    public $studentInfor = [];
 
     public function whereRegNum($csvRegNo)
     {
         $data = Student::where('reg_num', $csvRegNo);
 
-        array_push($this->studentInfo, collect($data)->toArray());
+        array_push($this->studentInfor, collect($data)->toArray());
         //for everytime it loops through the excel rows, it returns the first/or it returns a single row
         return $data->exists() ? $data->first() : 'No student Record found';
     }
     //so I am trying to prevent the function regnum from being called multiple times instead, I push all the data into the StendInfo array then find the matching gone
     //and return it.
+
+    // to make upload of result a little bit perfect to some extent, I will need to make sure that the admin selects the session he/she is uploading for
+    //the term too
+
+    //school need to be related with term...so at one point I know the term that class is or abi school 
 
     public function collection(collection $rows)
     {
@@ -39,14 +44,14 @@ class ResultsImport implements ToCollection
         foreach ($t as $key => $row) {
 
             if ($this->whereRegNum($row[2]) != 'No student Record found') {
-                dd($this->studentInfo);
+                dd($this->studentInfor);
                 Result::updateOrCreate(
                     ['student_id' => $this->whereRegNum($row[2])], //I make the query once then push the result to an array so I avoid repeating them
 
                     [
-                        'class_id' => $this->studentInfo[$key]['class_id'],
+                        'class_id' => $this->studentInfor[$key]['class_id'],
                         'subject_id' => $row[4], // we will want to make sure this subject_id here is same with the id column for subjects in the subjectsTable !!!
-                        'school_id' => $this->studentInfo[$key]['school_id'],
+                        'school_id' => $this->studentInfor[$key]['school_id'],
                         'assessment_total' => $row[6],
                         'exam_score' => $row[7],
                         'total_score' => $row[7] + $row[6],
--- resources/views/backend/result/upload.blade.php
+++ resources/views/backend/result/upload.blade.php
@@ -13,6 +13,23 @@
         <div class="card p-4">
             <form action="{{ route('dashboard.admin.importPost') }}" method="POST" enctype="multipart/form-data">
                 @csrf
+                <form>
+<span class="text-danger font-italic "> *  kindly note that you will need to <span class="font-bold">select</span> the <span class="font-bold"> right class</span> and term for this </span>
+                    <div class="row">
+                        <div class="form-group col-lg-6">
+                            <label for="exampleFormControlSelect1" class="font-weight-bold"><span class="text-danger">*</span> Select Class</label>
+                            <select name="class_id" value="1" class="form-control" id="exampleFormControlSelect1">
+                                <option value="$class->id ">$class->class_name </option>
+                            </select>
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label for="exampleFormControlSelect1" class="font-weight-bold"> <span class="text-danger">*</span> Select Term</label>
+                            <select name="Sub_Class_id" value="1" class="form-control" id="exampleFormControlSelect1">
+                                <option value=" $subclass->id"> stuff </option>
+                            </select>
+                        </div>
+                    </div>
+                </form>
                 <div class="custom-file ">
                     <input type="file" name="file" class="custom-file-input" id="customFileLang" lang="en">
                     <label class="custom-file-label" for="customFileLang">Select file</label>
