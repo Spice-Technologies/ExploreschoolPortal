e1e4393a26ae19c11ea37b41d71ae986c0045199
Chidiebere Chukwudi <chidideveloper@gmail.com>
2022-11-07 22:52:37 +0100

    mass promotion for student jss1 to ss3 working fine now

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -33,26 +33,35 @@ class PromotionController extends Controller
         //when you want to think about it, assume that i used mass update ->update([]) method
         //for what I know , it will be difficulut to get the class to add or pluss with 1
         // dd($massPromote == true ? 'true' : 'false');
-        if ($massPromote) {
 
-            //promote normal students...
+
+        //promote normal students... *** Also because we needed to retrieve all results first then Update records ****
+        $massPromote->each(function ($query) use ($currentSession) {
+            // implemeting laravel streaming results lazily then updated records while iterating over them
+            //perfomance booster
+            DB::table('students')
+                ->where('id', $query->id)
+                ->update([
+                    'class_id' => $query->class_id + 1,
+                    'current_session' => $currentSession->session,
+                    'session_id' => $currentSession->id
+                ]);
+        });
+
+        return redirect()->route('student.index')->with('msg', 'Students promoted successfully');
+
+        if (!$massPromote) {
+            // promote graduate
             $massPromote->each(function ($query) use ($currentSession) {
-
-                $query->class_id = $query->class_id + 1;
-                $query->current_session =   $currentSession->session;
-                $query->session_id = $currentSession->id;
-                $query->save();
+                DB::table('students')
+                    ->where('id', $query->id)
+                    
+                    ->update([
+                        'graduate_status' => 1,
+                        'current_session' =>  $currentSession->session,
+                        'session_id' => $currentSession->id
+                    ]);
             });
-
-            return redirect()->route('student.index')->with('msg', 'Students promoted successfully');
-        } else {
-            //promote graduate
-            $massPromote->update([
-                'graduate_status' => 1,
-                'current_session' =>  $currentSession->session,
-                'session_id' => $currentSession->id
-            ]);
-            return redirect()->route('student.index')->with('msg', 'Students promoted successfully');
         }
     }
 }
