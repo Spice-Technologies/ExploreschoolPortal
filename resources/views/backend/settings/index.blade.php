af283e445cd4ff8a78ff39090129549275d4c388
Chidiebere Chukwudi <chidideveloper@gmail.com>
2022-11-17 16:47:34 +0100

    create page completed for settings page

--- resources/views/backend/settings/index.blade.php
+++ resources/views/backend/settings/index.blade.php
@@ -18,9 +18,6 @@
 
                         <h3 class="mb-0">Settings: Adjust School Info </h3>
                     </div>
-                    <div class="col-4 text-right">
-                        <a href="#!" class="btn btn-sm btn-primary">Upload Excel File</a>
-                    </div>
                 </div>
             </div>
             <div class="card-body">
@@ -38,9 +35,9 @@
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <label class="form-control-label" for="input-email">Last Name</label>
-                                    <input type="text" id="input-email" value="{{ old('lname') }}" name="lname"
-                                        class="form-control" placeholder="last name">
+                                    <label class="form-control-label" for="input-email">Phone Number</label>
+                                    <input type="text" id="input-email" value="{{ old('number') }}" name="number"
+                                        class="form-control" placeholder="Enter phone number">
                                 </div>
                             </div>
 
@@ -65,74 +62,47 @@
                         </div>
                         <hr class="my-4" />
                         <!--Class details -->
-                        <h6 class="heading-small text-muted mb-4">Class details</h6>
+                        <h3 class="heading-small text-muted mb-4">Assign Form Teacher to each class</h3>
                         <div class="row">
                             <div class="form-group col-lg-6">
-                                <label for="exampleFormControlSelect1">Asign Class</label>
-                                <select name="class_id" value="1" class="form-control" id="exampleFormControlSelect1">
-                                    {{-- @foreach ($classes as $class) --}}
-                                    <option value=""></option>
-                                    {{-- @endforeach --}}
-                                </select>
+                                <label class="form-control-label" for="input-username">JSS 1</label>
+                                <input type="text" id="lname" value="{{ old('jss1') }}" name="jss1"
+                                    class="form-control" placeholder="jss 1 form teacher name ">
                             </div>
                             <div class="form-group col-lg-6">
-                                <label for="exampleFormControlSelect1">Asign Sub Class</label>
-                                <select name="Sub_Class_id" value="1" class="form-control"
-                                    id="exampleFormControlSelect1">
-                                    {{-- @foreach ($subclasses as $subclass) --}}
-                                    <option value=""></option>
-                                    {{-- @endforeach --}}
-                                </select>
+
+                                <label class="form-control-label" for="input-username">JSS 2</label>
+                                <input type="text" id="lname" value="{{ old('jss2') }}" name="jss2"
+                                    class="form-control" placeholder="jss 2 form teacher name ">
                             </div>
                         </div>
-                        <hr class="my-4" />
-                        <!-- Address -->
-                        <h6 class="heading-small text-muted mb-4">Contact information</h6>
-                        <div class="pl-lg-4">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label class="form-control-label" for="input-address">Current Address</label>
-                                        <input id="input-address" name="current_address" class="form-control"
-                                            placeholder="Home Address"
-                                            value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" type="text">
-                                    </div>
-                                </div>
+                        <div class="row">
+                            <div class="form-group col-lg-6">
+                                <label class="form-control-label" for="input-username">JSS 3</label>
+                                <input type="text" id="lname" value="{{ old('jss3') }}" name="jss3"
+                                    class="form-control" placeholder="jss 3 form teacher name ">
                             </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label class="form-control-label" for="input-address">Permanent Address</label>
-                                        <input id="input-address" name="permanent_address" class="form-control"
-                                            placeholder="Home Address" value="amaudara umualumkau " type="text">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-4">
-                                    <div class="form-group">
-                                        <label class="form-control-label" for="input-city">State of Origin </label>
-                                        <input type="text" name="state" value="{{ old('state') }}"
-                                            class="form-control" placeholder="Country">
-                                    </div>
-                                </div>
-                                <div class="col-lg-4">
-                                    <div class="form-group">
-                                        <label class="form-control-label" for="input-country">LGA</label>
-                                        <input type="text" name="lga" value="{{ old('lga') }}"
-                                            class="form-control" placeholder="LGA">
-                                    </div>
-                                </div>
-                                <div class="col-lg-4">
-                                    <div class="form-group">
-                                        <label class="form-control-label" for="input-country">Country</label>
-                                        <input type="text" name="country" value="{{ old('country') }}"
-                                            class="form-control" placeholder="Postal code">
-                                    </div>
-                                </div>
+                            <div class="form-group col-lg-6">
+
+                                <label class="form-control-label" for="input-username">SSS 1</label>
+                                <input type="text" id="lname" value="{{ old('ss1') }}" name="ss1"
+                                    class="form-control" placeholder="sss 1 form teacher name ">
                             </div>
                         </div>
-                        <hr class="my-4" />
+                        <div class="row">
+                            <div class="form-group col-lg-6">
+                                <label class="form-control-label" for="input-username">SSS 2</label>
+                                <input type="text" id="lname" value="{{ old('ss2') }}" name="ss2"
+                                    class="form-control" placeholder="sss 2 form teacher name ">
+                            </div>
+                            <div class="form-group col-lg-6">
+
+                                <label class="form-control-label" for="input-username">SSS 3</label>
+                                <input type="text" id="lname" value="{{ old('ss3') }}" name="ss3"
+                                    class="form-control" placeholder="sss 3 form teacher name ">
+                            </div>
+                        </div>
+
                         <div class="">
                             <button class="btn btn-primary" type="submit">Submit </button>
                         </div>
